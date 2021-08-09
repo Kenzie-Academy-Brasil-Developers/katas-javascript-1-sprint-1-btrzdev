ac642d0a8c4e924468fe3e29e900ceec2548a525
Beatriz Silva <btrz.dev@gmail.com>
2021-08-09 10:11:17 -0300

    final resolution

--- katas1.js
+++ katas1.js
@@ -12,8 +12,127 @@ function oneThroughTwenty() {
 
 
 
- 
+function evensToTwenty() {  
+   let num = 0; 
+      while(num <= 20) {
+      if ((num % 2 === 0)) {
+         console.log(num)       
+      }
+      num++;
+   }  
+   }; 
+   evensToTwenty();
 
+
+
+
+
+function oddsToTwenty() {
+   let num = 0; 
+      while(num <= 20) {
+      if ((num % 2 === 1)) {
+         console.log(num)       
+      }
+      num++;
+   }  
+   }; 
+   oddsToTwenty();
+
+
+
+
+function multiplesOfFive() {    
+    let num = 5; 
+    while(num <= 100) {
+      if ((num % 5 === 0)) {
+        console.log(num)       
+      }
+     num++;
+   }  
+  }; 
+   multiplesOfFive();
+
+
+
+
+function squareNumbersBackwards() {
+   let num = 1; 
+   while(num <= 100) {
+      if (Number.isInteger(Math.sqrt(num)) == true){
+         console.log(num)       
+      }
+      num++;
+   }
+   };
+   squareNumbersBackwards();
+
+
+
+
+function countingBackwards() {
+   let resultado= [];  
+   for (let counter = 20; counter >= 1; counter--) {
+      resultado.push(counter);
+   }
+   return resultado;  
+   };
+   countingBackwards();
+
+
+
+
+function evenNumbersBackwards() {
+   let num = 20; 
+   while(num >= 1) {
+     if ((num % 2 === 0)) {
+       console.log(num)       
+     }
+    num--;
+  }  
+ }; 
+   evenNumbersBackwards();
+   
+
+
+
+function oddNumbersBackwards() {
+   let num = 20; 
+   while(num >= 1) {
+      if ((num % 2 === 1)) {
+         console.log(num)       
+      }
+      num--;
+   }  
+   };
+   oddNumbersBackwards(); 
+
+
+
+
+function multiplesOfFiveBackwards() {
+   let num = 100; 
+   while(num >= 5) {
+      if ((num % 5 === 0)) {
+         console.log(num)       
+      }
+      num--;
+   }  
+   };
+   multiplesOfFiveBackwards();  
+
+
+
+ 
+function squareNumbersBackwards() {
+   let num = 100; 
+   while(num >= 0) {
+      if (Number.isInteger(Math.sqrt(num)) == true){
+         console.log(num)       
+      }
+      num--;
+   }
+   };
+   squareNumbersBackwards();
 
 
 
--- katas6.js
+++
@@ -1,8 +0,0 @@
-function countingBackwards() {
-    let resultado= [];  
-    for (let counter = 20; counter >= 1; counter--) {
-       resultado.push(counter);
-    }
-    return resultado;  
-  };
-    countingBackwards();
